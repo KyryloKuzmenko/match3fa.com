a88b35dffef75e328752e1873f5116ec30be3e81
Jango <kkuzmenko1992@gmail.com>
2025-07-09 00:07:27 -0230

    added basic markup

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.querySelector('.burger-menu');
+  const closeMenu = document.querySelector('.close-menu');
   const backdrop = document.querySelector('.backdrop');
-  const headerContent = document.querySelector('.header-section .header-content');
   const links = document.querySelectorAll('.backdrop-list-item');
-
-  menuBtn?.addEventListener('click', () => {
-    backdrop.classList.toggle('show');
-    headerContent.classList.toggle(backdrop.classList.contains('show'));
-  });
-
-  backdrop?.addEventListener('click', e => {
-    if (e.target === backdrop) {
-      backdrop.classList.remove('show');
-    }
-  });
 
   links.forEach(link => {
     link.addEventListener('click', () => {
       backdrop.classList.remove('show');
+      toggleMenu(false);
     });
+  });
+
+  function toggleMenu(show) {
+    menuBtn.style.display = show ? 'none' : 'block';
+    closeMenu.style.display = show ? 'block' : 'none';
+    backdrop.classList.toggle('show', show);
+  }
+
+  menuBtn?.addEventListener('click', () => toggleMenu(true));
+  closeMenu?.addEventListener('click', () => toggleMenu(false));
+  backdrop?.addEventListener('click', e => {
+    if (e.target === backdrop) toggleMenu(false);
   });
 });
